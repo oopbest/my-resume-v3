51dfff33ae8f8e5992779d4af3772ad2d79fc0fe
Setthawut Pimthong <oop_best@yahoo.com>
2025-04-03 10:08:19 +0700

    update badge

--- src/layout/components/PortfioGrid.tsx
+++ src/layout/components/PortfioGrid.tsx
@@ -9,8 +9,11 @@ import {
 import { Project } from "@/types";
 import { useState } from "react";
 import ImageCarousel from "./ImageCarousel";
+import { Badge } from "@/components/ui/badge";
+import { FaGithub } from "react-icons/fa";
+import { LuTvMinimalPlay } from "react-icons/lu";
 
-const MAX_LENGTH = 85;
+const MAX_LENGTH = 180;
 
 type Props = {
   project: Project;
@@ -41,12 +44,14 @@ const PortfioGrid = ({ project }: Props) => {
             className="absolute z-20 inset-0 h-full w-full object-cover transition-transform duration-300 group-hover:scale-105 group-hover:opacity-75"
           />
           <div className="absolute z-30 inset-0 hidden group-hover:flex items-center justify-center">
-            <Button onClick={() => openDialog(project.photos)}>See more</Button>
+            <Button onClick={() => openDialog(project.photos)}>
+              Screenshots
+            </Button>
           </div>
         </div>
       </div>
       <p>{project.title}</p>
-      <p className="text-sm text-zinc-400 mt-2">
+      <p className="text-sm text-zinc-400 my-4">
         {expanded || project.description.length <= MAX_LENGTH
           ? project.description
           : `${project.description.slice(0, MAX_LENGTH)}... `}
@@ -59,18 +64,24 @@ const PortfioGrid = ({ project }: Props) => {
           </span>
         )}
       </p>
-      {project.stacks.map((stack, index) => (
-        <p key={index} className="text-xs mt-4">
-          {stack}
-        </p>
-      ))}
+      <div className="flex flex-wrap gap-2">
+        {project.stacks.map((stack, index) => (
+          <Badge key={index} variant={"secondary"}>
+            {stack}
+          </Badge>
+        ))}
+      </div>
       <div className="flex items-end justify-center mt-6 gap-2">
         <a href={project.projectUrl} target="_blank" rel="noreferrer">
-          <Button variant={"link"}>Visit Site</Button>
+          <Button>
+            <LuTvMinimalPlay className="h-5 w-5" /> Live Demo
+          </Button>
         </a>
         {project.projectSrc && (
           <a href={project.projectSrc} target="_blank" rel="noreferrer">
-            <Button variant={"link"}>Source Code</Button>
+            <Button>
+              <FaGithub className="h-5 w-5" /> View Code
+            </Button>
           </a>
         )}
       </div>
--- src/layout/components/Portfolio.tsx
+++ src/layout/components/Portfolio.tsx
@@ -7,7 +7,17 @@ const projects = [
     description:
       "A real-time music streaming application inspired by Spotify. It offers features such as music playback with next and previous song navigation, volume control via a slider, an admin dashboard for managing albums and songs, integrated real-time chat, online/offline status indicators, and the ability to see what other users are listening to in real-time.",
     stacks: [
-      "React, Typescript, Zustand, Tailwind CSS, shadcn/ui, Node.js, Express, MongoDB, Mongoose, Socket.io, Cloudinary",
+      "React",
+      "Typescript",
+      "Zustand",
+      "Tailwind CSS",
+      "shadcn/ui",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Mongoose",
+      "Socket.io",
+      "Cloudinary",
     ],
     photos: [
       "/project-5/ss-1.png",
@@ -30,7 +40,15 @@ const projects = [
     description:
       "Topvalue.com is an e-commerce website offering a variety of products, including electronics, home appliances, and lifestyle goods. The website features a user-friendly interface, secure payment processing, and a customer support system.",
     stacks: [
-      "Next.js, Typescript, Redux Toolkit, Bootstrap, NextAuth.js, REST API, PHP (Magento 2), MySQL, Google Cloud Platform",
+      "Next.js",
+      "Typescript",
+      "Redux Toolkit",
+      "Bootstrap",
+      "NextAuth.js",
+      "REST API",
+      "PHP (Magento 2)",
+      "MySQL",
+      "Google cloud platform",
     ],
     photos: [
       "/project-4/ss-1.png",
@@ -50,7 +68,17 @@ const projects = [
     description:
       "A real-time chat application built using the MERN stack. This application supports features such as one-on-one chats, user search functionality, and real-time notifications.",
     stacks: [
-      "React, Zustand, useContext, Tailwind CSS, Daisy UI, Node.js, Express, MongoDB, Mongoose, Socket.io, JWT",
+      "React",
+      "Zustand",
+      "useContext",
+      "Tailwind CSS",
+      "Daisy UI",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Mongoose",
+      "Socket.io",
+      "JWT",
     ],
     photos: [
       "/project-3/ss-1.png",
@@ -67,7 +95,17 @@ const projects = [
     description:
       "Real-time messaging, theme switch, image uploads, authentication, cloud hosting on Render.com",
     stacks: [
-      "React, Zustand, Tailwind CSS, Daisy UI, Node.js, Express, MongoDB, Mongoose, Cloudinary, Socket.io, JWT",
+      "React",
+      "Zustand",
+      "Tailwind CSS",
+      "Daisy UI",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Mongoose",
+      "Cloudinary",
+      "Socket.io",
+      "JWT",
     ],
     photos: [
       "/project-2/ss-1.png",
@@ -85,7 +123,7 @@ const projects = [
     title: "Full-stack a Product Store & Deployment",
     description:
       "A full-stack web application built using the MERN stack—MongoDB, Express.js, React.js, and Node.js. It serves as a beginner-friendly crash course for learning how to develop full-stack applications with these technologies. The application allows users to perform CRUD (Create, Read, Update, Delete) operations on product data through a user-friendly interface.",
-    stacks: ["React, Zustand, Chakra UI, Node.js, MongoDB, Mongoose"],
+    stacks: ["React", "Zustand", "Chakra UI", "Node.js", "MongoDB", "Mongoose"],
     photos: [
       "/project-1/ss-1.png",
       "/project-1/ss-2.png",
@@ -101,7 +139,7 @@ const Portfolio = () => {
   return (
     <div id="portfolio" className="p-2">
       <h2 className="text-center text-3xl font-bold py-8">Portfolio</h2>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 ">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 ">
         {projects.map((project, index) => (
           <PortfioGrid key={index} project={project} />
         ))}
