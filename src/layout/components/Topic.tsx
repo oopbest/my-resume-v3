76b9056bee24661da10089ada0010cef5214bc2b
Setthawut Pimthong <oop_best@yahoo.com>
2025-04-16 17:13:00 +0700

    update topic gradient

--- src/layout/components/Topic.tsx
+++ src/layout/components/Topic.tsx
@@ -4,7 +4,15 @@ type Props = {
 
 const Topic = ({ title }: Props) => {
   return (
-    <h2 className="text-center text-3xl font-bold py-8 uppercase">{title}</h2>
+    <div className="mb-10 relative text-center">
+      <h2 className="text-3xl font-bold uppercase relative z-10 pt-10 inline-block">
+        {title}
+      </h2>
+
+      {/* Gradient line */}
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 w-60 h-[2px] bg-gradient-to-r from-transparent via-indigo-500 to-transparent blur-sm" />
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 w-60 h-px bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </div>
   );
 };
 
